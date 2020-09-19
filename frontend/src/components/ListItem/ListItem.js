e2907a39637816ab9d4ccd657a7b5da219e82a1e
metzlr <reedmg15@gmail.com>
2020-09-19 16:30:36 -0700

    Created a basic ListItem component and updated the list to use it

---
+++ frontend/src/components/ListItem/ListItem.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import { Container, Row, ListGroupItem, Button } from "react-bootstrap";
+
+const ListItem = props => {
+    const { itemName, itemQty, claimedName } = props;
+    
+    return (
+        <ListGroupItem>
+            <div className="d-flex justify-content-between align-items-center">
+                <span><strong>{itemName}</strong>{itemQty > 1 ? ` (${itemQty})` : null}</span>
+
+                {claimedName == undefined ?
+                    (
+                        <Button variant="secondary" onClick={() => { console.log(`Claim button clicked for ${itemName}`)}}>Claim</Button>
+                    ) :
+                    (
+                        <i>{claimedName}</i>
+                    )
+                }
+            </div>
+        </ListGroupItem>  
+    )
+
+}
+
+export default ListItem;
