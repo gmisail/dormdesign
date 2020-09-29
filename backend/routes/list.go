56b7266d91667d0feb741f52d20ae650cfca77c4
Graham Misail <gmisail64@gmail.com>
2020-09-29 14:56:55 -0400

    Added expected request structure

--- backend/routes/list.go
+++ backend/routes/list.go
@@ -11,12 +11,31 @@ type ListRoute struct {
 	Database *redis.Client
 }
 
+/*
+
+Creates an empty list for a given ID.
+
+{
+	id: string
+}
+
+*/
 func (route *ListRoute) OnCreateList(c echo.Context) error {
 	models.CreateList(route.Database, "testtesttest")
 
 	return c.String(http.StatusOK, "new list")
 }
 
+/*
+
+{
+	id: string
+	name: string
+	quantity: int
+	claimedBy: string
+}
+
+*/
 func (route *ListRoute) OnAddListItem(c echo.Context) error {
 	models.AddListItem(route.Database, "testtesttest", models.ListItem{ Name: "Another", Quantity: 3, ClaimedBy: "graham" })
 
