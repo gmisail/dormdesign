8ddd5abe106f9cb851da71f5692b2657cffd6b5c
Graham Misail <gmisail64@gmail.com>
2021-10-15 16:56:32 -0400

    Add condition if .env doesn't exist

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,11 @@
+sudo docker-compose down
+
 if [ -f .env ]
 then
   echo "[DormDesign] .env file found."
+else
+  echo "[DormDesign] Cannot deploy without .env file."
+  exit 1
 fi
 
 echo "[DormDesign] Starting server."
