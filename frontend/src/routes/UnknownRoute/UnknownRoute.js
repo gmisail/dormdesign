055e680e48c1a4c147adc12a3014cd9018f381a6
Graham Misail <gmisail64@gmail.com>
2021-10-27 23:10:15 -0400

    Formatting

--- frontend/src/routes/UnknownRoute/UnknownRoute.js
+++ frontend/src/routes/UnknownRoute/UnknownRoute.js
@@ -2,17 +2,16 @@ import { Link } from "react-router-dom";
 import "./UnknownRoute.scss";
 
 export const UnknownRoute = () => {
-    return (
-        <div className="content-wrapper">
-            <div className="unknown-container">
-                <h1 className="unknown_title">
-                    404
-                </h1>
+  return (
+    <div className="content-wrapper">
+      <div className="unknown-container">
+        <h1 className="unknown_title">404</h1>
 
-                <p className="unknown_desc">
-                    Unfortunately, we could not find the route that you were looking for. <Link to="/">Return to the home page?</Link>
-                </p>
-            </div>
-        </div>
-    );
-}
+        <p className="unknown_desc">
+          Unfortunately, we could not find the route that you were looking for.{" "}
+          <Link to="/">Return to the home page?</Link>
+        </p>
+      </div>
+    </div>
+  );
+};
