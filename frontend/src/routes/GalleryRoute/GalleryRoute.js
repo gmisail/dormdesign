684a67a072c5ee305e0d20b3ee17247944f9fa2b
Graham Misail <gmisail64@gmail.com>
2021-11-20 19:29:51 -0500

    Add gallery "pages" and relevant styles

--- frontend/src/routes/GalleryRoute/GalleryRoute.js
+++ frontend/src/routes/GalleryRoute/GalleryRoute.js
@@ -2,6 +2,7 @@ import "./GalleryRoute.scss";
 
 import React, { useCallback, useEffect } from "react";
 
+import SingleInputForm from "../../components/SingleInputForm/SingleInputForm"
 import GalleryItem from "../../components/GalleryItem/GalleryItem";
 
 export const GalleryRoute = () => {
@@ -9,7 +10,31 @@ export const GalleryRoute = () => {
 
   return (
     <div className="gallery-container">
-      <div className="gallery-toolbar"></div>
+      <div className="gallery-pages">
+        <button
+          className="custom-btn custom-btn-outline gallery-page gallery-btn-featured"
+          type="submit"
+        >
+          Featured
+        </button>
+        <button
+          className="custom-btn custom-btn-outline gallery-page gallery-btn-community"
+          type="submit"
+        >
+          Community
+        </button>
+      </div>
+      <div className="gallery-toolbar">
+        <SingleInputForm
+          initialValue={"Hello"}
+          submitButtonText={"Search"}
+          submitButtonTextSuccessful={"Search"}
+          onSubmit={(content) => console.log(content)}
+          trim={true}
+          allowEmptySubmit={false}
+          maxLength={40}
+        />
+      </div>
       <div className="gallery-grid">
         {rooms.map((room) => (
           <GalleryItem key={0} id={0} roomName={"Test " + 0} preview={""} />
