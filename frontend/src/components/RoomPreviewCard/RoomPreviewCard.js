3678e390a5c80d8f7d2f456958c7404b31a96a89
Reed <reedmg15@gmail.com>
2021-12-09 18:48:23 -0500

    Added some of the clear button styling

--- frontend/src/components/RoomPreviewCard/RoomPreviewCard.js
+++ frontend/src/components/RoomPreviewCard/RoomPreviewCard.js
@@ -2,15 +2,22 @@ import "./RoomPreviewCard.scss";
 
 import React from "react";
 import RoomPreview from "./RoomPreview";
+import IconButton from "../IconButton/IconButton";
+import { BsX } from "react-icons/bs";
 
 const RoomPreviewCard = (props) => {
   const { roomName, id, preview } = props;
 
   return (
-    <a href={`/room/${id}`} className="room-preview-card">
-      <span className="room-preview-name">{roomName}</span>
-      <RoomPreview preview={preview} />
-    </a>
+    <div className="room-preview-card">
+      <IconButton className="remove-button">
+        <BsX />
+      </IconButton>
+      <a href={`/room/${id}`} className="">
+        <span className="room-preview-name">{roomName}</span>
+        <RoomPreview preview={preview} />
+      </a>
+    </div>
   );
 };
 
