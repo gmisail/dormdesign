03a1f1b365bf1698bdcba83ab72e05cc4819d66b
Reed <reedmg15@gmail.com>
2022-02-01 16:36:53 -0500

    Finished remove room history button. Made some minor styling adjustments

--- frontend/src/components/RoomPreviewCard/RoomPreview.js
+++
@@ -1,7 +0,0 @@
-import React, { useEffect, useState } from "react";
-
-import DataRequests from "../../controllers/DataRequests";
-
-export default function RoomPreview({ preview }) {
-  return <img className="room-preview-image" src={preview} alt="Room Preview"></img>;
-}
