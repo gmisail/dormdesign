2b9835bf95bdcf0f5efcc48b63ff48ef6e213f6f
Graham Misail <gmisail64@gmail.com>
2022-05-21 13:51:21 -0400

    Finish up room cache test suite

--- server/src/tests/room.service.test.ts
+++ server/src/tests/room.service.test.ts
@@ -9,6 +9,25 @@ const INVALID_ROOM_ID = "does-not-exist";
 const Redis = require("ioredis-mock");
 const client = new Redis();
 
+const createItem = () => {
+  return {
+    id: "some-item-id",
+    name: "awesome item",
+    quantity: 1,
+    visibleInEditor: false,
+    claimedBy: "jest",
+    dimensions: {
+      width: 5,
+      height: 5,
+      length: 5,
+    },
+    editorPosition: { x: 10, y: 10 },
+    editorZIndex: 1,
+    editorRotation: 0,
+    editorLocked: false,
+  };
+};
+
 jest.mock("ioredis", () => {
   return function () {
     return client;
@@ -237,22 +256,7 @@ describe("Room cache", () => {
       },
     };
 
-    const item = {
-      id: "some-item-id",
-      name: "awesome item",
-      quantity: 1,
-      visibleInEditor: false,
-      claimedBy: "jest",
-      dimensions: {
-        width: 5,
-        height: 5,
-        length: 5,
-      },
-      editorPosition: { x: 10, y: 10 },
-      editorZIndex: 1,
-      editorRotation: 0,
-      editorLocked: false,
-    };
+    const item = createItem();
 
     const updatedItem = await RoomCacheService.addItem(ROOM_ID, item);
     expected.data.items.push(updatedItem);
@@ -266,5 +270,115 @@ describe("Room cache", () => {
     expected.metaData.lastModified = received.metaData.lastModified;
 
     expect(received).toStrictEqual(expected);
+  });
+
+  test("if all items can be cleared", async () => {
+    let original = {
+      id: ROOM_ID,
+      templateId: "template",
+      data: {
+        name: "Test Room",
+        items: [],
+        vertices: [
+          { x: -5, y: -5 },
+          { x: 5, y: -5 },
+          { x: 5, y: 5 },
+          { x: -5, y: 5 },
+        ],
+      },
+      metaData: {
+        featured: false,
+        totalClones: 0,
+        lastModified: Date.now(),
+      },
+    };
+
+    const item = createItem();
+
+    original.data.items.push(item);
+    original.data.items.push(item);
+
+    await client.set(ROOM_ID, JSON.stringify(original));
+    await RoomCacheService.clearItems(ROOM_ID);
+
+    const received = JSON.parse(await client.get(ROOM_ID)) as Room;
+
+    expect(received.data.items.length).toBe(0);
+    expect(received.data.items).toStrictEqual([]);
+  });
+
+  test("if an item can be removed", async () => {
+    let original = {
+      id: ROOM_ID,
+      templateId: "template",
+      data: {
+        name: "Test Room",
+        items: [],
+        vertices: [
+          { x: -5, y: -5 },
+          { x: 5, y: -5 },
+          { x: 5, y: 5 },
+          { x: -5, y: 5 },
+        ],
+      },
+      metaData: {
+        featured: false,
+        totalClones: 0,
+        lastModified: Date.now(),
+      },
+    };
+
+    const item = createItem();
+    original.data.items.push(item);
+
+    await client.set(ROOM_ID, JSON.stringify(original));
+    await RoomCacheService.removeItem(ROOM_ID, "some-item-id");
+
+    const received = JSON.parse(await client.get(ROOM_ID)) as Room;
+
+    expect(received.data.items.length).toBe(original.data.items.length - 1);
+    expect(received.data.items).not.toContainEqual(item);
+  });
+
+  test("if items can be updated", async () => {
+    let original = {
+      id: ROOM_ID,
+      templateId: "template",
+      data: {
+        name: "Test Room",
+        items: [],
+        vertices: [
+          { x: -5, y: -5 },
+          { x: 5, y: -5 },
+          { x: 5, y: 5 },
+          { x: -5, y: 5 },
+        ],
+      },
+      metaData: {
+        featured: false,
+        totalClones: 0,
+        lastModified: Date.now(),
+      },
+    };
+
+    const item = createItem();
+    item.claimedBy = "test-suite";
+    item.editorPosition = { x: 1.25, y: 8 };
+
+    original.data.items.push(item);
+
+    await client.set(ROOM_ID, JSON.stringify(original));
+    await RoomCacheService.updateItems(ROOM_ID, [
+      {
+        id: item.id,
+        updated: {
+          claimedBy: "test-suite",
+          editorPosition: { x: 1.25, y: 8 },
+        },
+      },
+    ]);
+
+    const received = JSON.parse(await client.get(ROOM_ID)) as Room;
+    expect(received.data.items[0]).toStrictEqual(original.data.items[0]);
   });
 });
